39cf30836fbbcb7aeecdc947c2390e06e215334e
Anthony Charles <anthony18meister@gmail.com>
2019-02-03 18:13:11 -0300

    deleting target folder

--- target/classes/db/migration/V1_0_0__create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id bigint,
-    name character varying(255) NOT NULL,
-    email character varying(255) NOT NULL,
-    password character varying(255) NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (id)
-);
